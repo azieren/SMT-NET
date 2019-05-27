7125ee11eb7a63eb1145fff8c4d2ece2208c9f74
azieren <39497320+azieren@users.noreply.github.com>
2019-05-27 15:29:18 -0700

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -17,39 +17,53 @@ def import_data(path, test = False):
 					if not test:
 						gt = int(l.pop(0))
 						if gt == 3:
-							label.append(1)
+							label.append([0,1])
 						else:
-							label.append(-1)
+							label.append([1,0])
 					data.append(l)
 						
 		return np.array(data), np.array(label)
 
 
-def network_train(x_train, y_train):
-	input_size = x_train.shape[1]
-	hidden_size = 1000
-	output_size = 1
-
-	x_train = Variable(torch.FloatTensor(x_train))
-	y_train = Variable(torch.FloatTensor(y_train))
-
-	model = nn.Sequential(nn.Linear(input_size, hidden_size),
-						  nn.ReLU(),
-						  nn.Linear(hidden_size, output_size),
-						  nn.Sigmoid())
-
-	criterion = torch.nn.MSELoss()
-
-	optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-	for epoch in range(1000):
-		y_pred = model(x_train)
-		loss = criterion(y_pred, y_train)
-		print('epoch: ', epoch, ' loss: ', loss.item())
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-
+def network_train(x_train, y_train, x_val, y_val):
+  input_size = x_train.shape[1]
+  hidden_size = 10
+  output_size = 2
+  
+  x_train = Variable(torch.FloatTensor(x_train))
+  y_train = Variable(torch.FloatTensor(y_train))
+  x_val = Variable(torch.FloatTensor(x_val))
+  y_val = Variable(torch.FloatTensor(y_val))
+  
+  model = nn.Sequential(nn.Linear(input_size, hidden_size),
+  						  nn.ReLU(),
+  						  nn.Linear(hidden_size, output_size),
+  						  nn.Softmax())
+  
+  criterion = torch.nn.BCELoss()
+  
+  optimizer = torch.optim.SGD(model.parameters(), lr=0.0000001)
+  
+  for epoch in range(1000):
+    optimizer.zero_grad()
+    y_pred = model(x_train)
+    loss = criterion(y_pred, y_train)
+    loss.backward()
+    optimizer.step()
+    
+    print('epoch: ', epoch, ' loss: ', loss.item())
+    if epoch % 10 == 0:
+        acc_train = evaluate(y_pred, y_train)
+        y_pred_val = model(x_val)
+        acc_val = evaluate(y_pred_val, y_val)
+        print('acc val: {}, train: {}'.format(acc_val.item(), acc_train.item()))
+ 
+def evaluate(Y, Y_gt):   
+    Y = torch.argmax(Y, dim = 1)
+    Y_gt = torch.argmax(Y_gt, dim = 1)
+    result = (Y == Y_gt.long()).sum().float()
+    return result/len(Y)
+        
 
 if __name__ == '__main__': 
 	# Load Data
@@ -60,4 +74,4 @@ if __name__ == '__main__':
 	x_train, y_train = import_data(path_train)
 	x_val, y_val = import_data(path_val)
 	x_test, _ = import_data(path_test, test = True)
-	network_train(x_train, y_train)
+	network_train(x_train, y_train, x_val, y_val)
