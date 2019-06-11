6b7cf07f157cf27df859ea72869646f03d91328d
azieren <39497320+azieren@users.noreply.github.com>
2019-06-10 19:50:18 -0700

    Update net2smt.py

--- net2smt.py
+++ net2smt.py
@@ -3,6 +3,10 @@ import numpy as np
 from pysmt.shortcuts import Symbol, And, GE, LT, Plus, Equals, Int, Real, Solver, Or, Times, Pow, NotEquals, Max
 from pysmt.shortcuts import is_sat, get_formula_size
 from pysmt.typing import INT, REAL, ArrayType
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 from network import import_data
 import network
 
@@ -137,7 +141,140 @@ class SMTNet:
         acc = 1.0 * (tn + tp) / (tp + tn + fp + fn)
         return acc
 
+def plot_clauses_time(x, y_time, title, hist = False, output = 'performances_time.png'):
+    plt.title(title)
+    if hist:
+        bins = np.arange(len(y_time))
+        plt.bar(bins, y_time, width=0.2, color = 'b', label='Time(s)')
+        plt.xticks(bins, x)
+    else:
+        plt.plot(x, y_time, '-bs', label='Time(s)')
+    plt.xlabel('Size')
+    plt.ylabel('Time')
+    plt.grid(True)
+    plt.tight_layout()
+    
+    plt.savefig(output)
+    plt.clf()
 
+def plot_accuracy(x, y_train, y_test, title, hist = False, output = 'accuracy.png'):
+    plt.title(title)
+    if hist:
+        bins = np.arange(len(y_test))
+        plt.bar(bins +0.2, y_train, width=0.2, color = 'r', label='Training')
+        plt.bar(bins-0.2, y_test, width=0.2, color = 'g', label='Test')
+        #plt.xticks(x)
+        #plt.hist(y_test, bins = x, label='Test')
+        plt.xticks(bins, x)
+    else:
+        plt.plot(x, y_train, '-r', label='Training')
+        plt.plot(x, y_test, '-g', label='Test')
+    plt.xlabel('Size')
+    plt.ylabel('Accuracy')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(output)
+    plt.clf()
+
+def test_data_size(x_train, y_train, x_val, y_val, h = [10]):
+    acc_train, acc_val = 0.0, 0.0
+    
+    size_list = range(50, 500, 50)
+    #size_list = range(1, 5)
+    N, n = x_train.shape
+    
+    total_time, total_acc, total_acc_test = [], [], [] 
+    for size in size_list:
+        smt_net = SMTNet(n, h)
+        
+        start_time = time.time()
+        formula = smt_net.feed_data(x_train[:size], y_train[:size])
+        regularize = smt_net.non_zero()
+        formula = And(formula, regularize)
+        smt_net.solve(formula)
+        end_time = time.time()
+        
+        total_time.append(end_time - start_time)
+                
+        for i,(w,b) in smt_net.net.items():
+            print("Weight: ", i, w)
+            print("Bias: ", i, b)
+    
+        acc_train = smt_net.test(x_train[:size], y_train[:size])
+        acc_val = smt_net.test(x_val, y_val)
+        total_acc.append(acc_train*100.0)
+        total_acc_test.append(acc_val*100.0)
+    
+        print("Train accuracy: {} \nValidation accuracy: {}\nTime: {}".format(acc_train, acc_val, end_time - start_time))
+    
+    h_string = '_'.join([str(x) for x in h])
+    output_t = 'performances_time_' + h_string + '.png'
+    output_acc = 'accuracy_' + h_string + '.png'
+    plot_clauses_time(size_list, total_time, "Time / input data", output = output_t)
+    plot_accuracy(size_list, total_acc, total_acc_test, "Perfomances / input data", output = output_acc)
+
+def test_net_size(x_train, y_train, x_val, y_val, size = 200):
+    acc_train, acc_val = 0.0, 0.0
+
+    #h_list = [[10], [50], [10,10], [10,50]]
+    h_list = [[10]]
+    x_train, y_train = x_train[:size], y_train[:size]
+
+    N, n = x_train.shape
+    print(N,n)
+    total_time, total_acc, total_acc_test = [], [], []
+    for h in h_list:
+        smt_net = SMTNet(n, h)
+        
+        start_time = time.time()
+        formula = smt_net.feed_data(x_train, y_train)
+        regularize = smt_net.non_zero()
+        formula = And(formula, regularize)        
+        smt_net.solve(formula)
+        end_time = time.time()
+        
+        total_time.append(end_time - start_time)
+        
+        for i,(w,b) in smt_net.net.items():
+            print("Weight: ", i, w)
+            print("Bias: ", i, b)
+    
+        #acc_train = smt_net.test(x_train, y_train)
+        acc_train = smt_net.test(x_train, y_train)
+        acc_val = smt_net.test(x_val, y_val)
+        total_acc.append(acc_train*100.0)
+        total_acc_test.append(acc_val*100.0)
+    
+        print("Train accuracy: {} \nValidation accuracy: {}\nTime: {} ".format(acc_train, acc_val, end_time - start_time))
+    
+    h_list = [ ",".join([str(x) for x in h]) for h in h_list ]
+    output_t = 'hist_performances_time_' + str(size) + '.png'
+    output_acc = 'hist_accuracy_' + str(size) + '.png'
+    plot_clauses_time(h_list, total_time, "Time / Architecture", hist = True, output = output_t)
+    plot_accuracy(h_list, total_acc, total_acc_test, "Perfomances / Architecture", hist = True, output = output_acc)
+
+def transfer_weights(x_train, y_train, x_val, y_val):
+    x_train, y_train = x_train, y_train
+    N, n = x_train.shape
+    h = [10]
+    
+    smt_net = SMTNet(n, h)
+    formula = smt_net.feed_data(x_train, y_train)
+    regularize = smt_net.non_zero()
+    #regularize = smt_net.regularize()
+    formula = And(formula, regularize)   
+    smt_net.solve(formula)
+    
+    weights, biases = [], []
+    for i,(w,b) in smt_net.net.items():
+        print("Weight: ", i, w)
+        print("Bias: ", i, b)
+        weights.append(w)
+        biases.append(b)
+        
+    main_network(weights, biases)
+    
 if __name__ == '__main__':
     acc_train, acc_val = 0.0, 0.0
     path_train = os.path.join(os.path.dirname(__file__), 'pa2_train.csv')
